1b5be7aa2f7a01f422b137f74b772e5e1ed7d530
Arjuna Nayak <106763322+arjunn881@users.noreply.github.com>
2022-10-06 00:25:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Table from "./tble/index";
+import "./tble/index.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Table />
+    </div>
+  );
+}
